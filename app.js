d431ab00cbbc60afb819f291f05d8fe4dbf777d2
Cynder97 <150056594+Cynder97@users.noreply.github.com>
2025-04-07 14:42:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
-const dbURI = "mongodb+srv://SDEV255:GmbnN6rUiMWcK7NT@testcluster.xh7xg.mongodb.net/node-tuts";
+const dbURI = "mongodb+srv://sbessenbacher:JuiHY5MflyDuixcs@nodetut.s7ttp06.mongodb.net/?retryWrites=true&w=majority&appName=Nodetut";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
