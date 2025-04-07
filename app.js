027d56f3345d422a383223e9c47a804db98f5087
Cynder97 <150056594+Cynder97@users.noreply.github.com>
2025-04-07 14:29:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
-const dbURI = 'mongodb+srv://sbessenbacher:JuiHY5MflyDuixcs@nodetutorial.tsrinsp.mongodb.net/?retryWrites=true&w=majority&appName=Nodetutorial'
-mongoose.connect(bdURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(result => app.listen(3000))
-    .catch(err => console.log(err));
+const dbURI = "mongodb+srv://SDEV255:GmbnN6rUiMWcK7NT@testcluster.xh7xg.mongodb.net/node-tuts";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-    res.locals.path = req.path;
-    next();
+  res.locals.path = req.path;
+  next();
 });
 
 app.get('/', (req, res) => {
-    res.redirect('/blogs');
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', {title: 'About'});
+  res.render('about', { title: 'About' });
 });
 
 app.use('/blogs', blogRoutes);
 
 app.use((req, res) => {
-    res.status(404).render('404', {title: '404'});
+  res.status(404).render('404', { title: '404' });
 });
